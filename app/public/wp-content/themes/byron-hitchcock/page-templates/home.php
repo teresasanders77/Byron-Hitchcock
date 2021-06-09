1df0172ec3e7dc342c7e4ef15e476f1cfc493b89
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-06-08 18:12:48 -0600

    added slider for home page

--- app/public/wp-content/themes/byron-hitchcock/page-templates/home.php
+++ app/public/wp-content/themes/byron-hitchcock/page-templates/home.php
@@ -19,19 +19,26 @@ get_header();
       </blockquote>
     </div>
     <div class="entry-content">
-
       <div class="content-wrapper">
         <div class="bio-teaser">
-
-          <h2>A native of Wyoming, Byron Hitchcock has performed across the United States, Latin America, and Asia to critical acclaim.</h2>
-
-          <p>Praised by the Boston Globe for his “fearlessly expressive” playing and his “finely honed tone,” Mr. Hitchcock has established himself as a dynamic and versatile artist, appearing on the concert stage as a concertmaster, soloist, and chamber musician. In 2019, Mr. Hitchcock assumed the role of Concertmaster of both the Opera Colorado Orchestra in Denver, Colorado, and of the Central City Opera Orchestra in Central City, Colorado.</p>
-
-          <a href="/about" class="button-small" role="button">
-            <span class="button-content-wrapper">
-              <span class="button-text">READ MORE</span>
-            </span>
-          </a>
+          <?php if (get_field('text')) : ?>
+            <?php the_field('text'); ?>
+          <?php endif; ?>
+          <div id="container">
+            <button class="learn-more">
+              <a href="/about">
+                <span class="circle" aria-hidden="true">
+                  <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">READ MORE</span>
+              </a>
+            </button>
+          </div>
+        </div>
+        <div class="slider">
+          <?php
+          echo do_shortcode('[smartslider3 slider="2"]');
+          ?>
         </div>
       </div>
     </div><!-- .entry-content -->
