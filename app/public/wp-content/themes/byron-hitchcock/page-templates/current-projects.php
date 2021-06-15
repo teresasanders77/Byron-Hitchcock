79b146a8baa349b2a9a561f2b6a2df11a85b4caf
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-06-14 18:23:50 -0600

    Started current-projects page, added grid cards for content and styled

--- app/public/wp-content/themes/byron-hitchcock/page-templates/current-projects.php
+++ app/public/wp-content/themes/byron-hitchcock/page-templates/current-projects.php
@@ -9,13 +9,52 @@ $site_url = get_site_url();
 get_header();
 ?>
 
-<section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<body class="current-projects-body">
+  <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-  <div class="entry-content">
-  </div><!-- .entry-content -->
+    <div class="entry-content">
+      <div class="current-projects">
+        <div class="card">
+          <div class="layer"></div>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus ac leo hendrerit porta sed sit amet purus. Sed accumsan sem ut orci euismod, at volutpat mauris pellentesque. Fusce quam nunc, iaculis non ligula eget, fringilla maximus sapien. Etiam ac sagittis massa, eu accumsan risus. Nulla aliquam imperdiet ante, eleifend porta orci sollicitudin nec.</p>
+            <div class="image">
+              <img src="../images/Byron-Logo-Color.png">
+            </div>
+            <div class="details">
+              <h2>Someone Famous<br><span>Website Designer</span></h2>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="layer"></div>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus ac leo hendrerit porta sed sit amet purus. Sed accumsan sem ut orci euismod, at volutpat mauris pellentesque. Fusce quam nunc, iaculis non ligula eget, fringilla maximus sapien. Etiam ac sagittis massa, eu accumsan risus. Nulla aliquam imperdiet ante, eleifend porta orci sollicitudin nec.</p>
+            <div class="image">
+              <img src="../images/Byron-Logo-Black.png">
+            </div>
+            <div class="details">
+              <h2>Someone Famous<br><span>Website Designer</span></h2>
+            </div>
+          </div>
+        </div>
+        <div class="card">
+          <div class="layer"></div>
+          <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus ac leo hendrerit porta sed sit amet purus. Sed accumsan sem ut orci euismod, at volutpat mauris pellentesque. Fusce quam nunc, iaculis non ligula eget, fringilla maximus sapien. Etiam ac sagittis massa, eu accumsan risus. Nulla aliquam imperdiet ante, eleifend porta orci sollicitudin nec.</p>
+            <div class="image">
+              <img src="../images/Byron-Logo-White.png">
+            </div>
+            <div class="details">
+              <h2>Someone Famous<br><span>Website Designer</span></h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- .entry-content -->
 
-</section><!-- #post-<?php the_ID(); ?> -->
+  </section><!-- #post-<?php the_ID(); ?> -->
 
-<?php
-get_footer();
-?>
+  <?php
+  get_footer();
+  ?>
