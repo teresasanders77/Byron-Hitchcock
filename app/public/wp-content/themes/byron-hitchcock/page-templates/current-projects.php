9eb9429e99789930c505232962aee84e917ce65a
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-06-28 17:28:08 -0600

    Finished grid for current projects

--- app/public/wp-content/themes/byron-hitchcock/page-templates/current-projects.php
+++ app/public/wp-content/themes/byron-hitchcock/page-templates/current-projects.php
@@ -9,64 +9,115 @@ $site_url = get_site_url();
 get_header();
 ?>
 
-<header class="banner">
-  <div class="header-text">
-    <button id="menu-toggle" aria-label="Menu" aria-expanded="false" aria-controls="menu" style="margin-top: 0"></button>
-    <h1>Current Projects</h1>
-    <a href="/"> <img class="brand" src="<?php echo get_template_directory_uri(); ?>/images/Byron-Logo-White.png" width="100" alt="Byron Logo"></a>
-  </div>
+<header class="banner current">
+  <button id="menu-toggle" style="margin-top: 2%" aria-label="Menu" aria-expanded="false" aria-controls="menu" style="margin-top: 0"></button>
+  <a href="/"> <img class="brand" src="<?php echo get_template_directory_uri(); ?>/images/Byron-Logo-White.png" width="100" alt="Byron Logo"></a>
 </header>
-
 
 <body class="current-projects-body">
   <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
     <div class="entry-content">
-      <div class="top-section">
-        <?php
-        $image = get_field('image');
-        $size = 'custom size';
-        if ($image) {
-          echo wp_get_attachment_image($image, $size);
-        }
-        ?>
+      <div class="header-text">
+        <h1>What I'm Working On</h1>
       </div>
-      <div class="current-projects">
-        <div class="card">
-          <div class="layer"></div>
-          <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus ac leo hendrerit porta sed sit amet purus. Sed accumsan sem ut orci euismod, at volutpat mauris pellentesque. Fusce quam nunc, iaculis non ligula eget, fringilla maximus sapien. Etiam ac sagittis massa, eu accumsan risus. Nulla aliquam imperdiet ante, eleifend porta orci sollicitudin nec.</p>
-            <div class="image">
-              <img src="../images/Byron-Logo-Color.png">
-            </div>
-            <div class="details">
-              <h2>Someone Famous<br><span>Website Designer</span></h2>
-            </div>
-          </div>
+      <div class="top-section">
+        <div class="mobile">
+          <?php
+          $image = get_field('image');
+          $size = 'custom size';
+          if ($image) {
+            echo wp_get_attachment_image($image, $size);
+          } ?>
         </div>
-        <div class="card">
-          <div class="layer"></div>
-          <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus ac leo hendrerit porta sed sit amet purus. Sed accumsan sem ut orci euismod, at volutpat mauris pellentesque. Fusce quam nunc, iaculis non ligula eget, fringilla maximus sapien. Etiam ac sagittis massa, eu accumsan risus. Nulla aliquam imperdiet ante, eleifend porta orci sollicitudin nec.</p>
-            <div class="image">
-              <img src="../images/Byron-Logo-Black.png">
-            </div>
-            <div class="details">
-              <h2>Someone Famous<br><span>Website Designer</span></h2>
-            </div>
-          </div>
+        <div class="desktop">
+          <?php
+          $image_desktop = get_field('image2');
+          $size = 'custom size';
+          if ($image_desktop) {
+            echo wp_get_attachment_image($image_desktop, $size);
+          }
+          ?>
         </div>
-        <div class="card">
-          <div class="layer"></div>
-          <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis purus ac leo hendrerit porta sed sit amet purus. Sed accumsan sem ut orci euismod, at volutpat mauris pellentesque. Fusce quam nunc, iaculis non ligula eget, fringilla maximus sapien. Etiam ac sagittis massa, eu accumsan risus. Nulla aliquam imperdiet ante, eleifend porta orci sollicitudin nec.</p>
-            <div class="image">
-              <img src="../images/Byron-Logo-White.png">
-            </div>
-            <div class="details">
-              <h2>Someone Famous<br><span>Website Designer</span></h2>
+      </div>
+      <div class="projects-wrapper">
+        <div class="current-projects">
+          <div class="card">
+            <div class="layer"></div>
+            <div class="content">
+              <p>
+                <?php if (get_field('text')) : ?>
+                  <?php the_field('text'); ?>
+                <?php endif; ?>
+              </p>
             </div>
           </div>
+          <div class="card">
+            <div class="layer"></div>
+            <div class="content">
+              <p>
+                <?php if (get_field('text_2')) : ?>
+                  <?php the_field('text_2'); ?>
+                <?php endif; ?>
+              </p>
+            </div>
+          </div>
+          <h2 class="recital-title">RECITAL</h2>
+          <p class="recital-title recital-description">Originally planned as a full recital for the public in Denver, at Englewood Arts Center, this has now morphed into a recording session to be shared online. The rep is now smaller, but I'm looking forward to some high-quality recordings being made.</p>
+          <h2 class="recital-title engelwood">ENGELWOOD RECORDING SESSION</h2>
+          <div class="card">
+            <div class="layer"></div>
+            <div class="content">
+              <p>
+                <?php if (get_field('text_3')) : ?>
+                  <?php the_field('text_3'); ?>
+                <?php endif; ?>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="layer"></div>
+            <div class="content">
+              <p>
+                <?php if (get_field('text_4')) : ?>
+                  <?php the_field('text_4'); ?>
+                <?php endif; ?>
+              </p>
+            </div>
+          </div>
+          <h2 class="recital-title">RECITAL REJECTS</h2>
+          <p class="recital-title recital-description">
+            These are the pieces that got tossed out of the recital project, but I’ve made my own mini-projects out of all of them!</p>
+          <div class="card">
+            <div class="layer"></div>
+            <div class="content">
+              <p>
+                <?php if (get_field('text_5')) : ?>
+                  <?php the_field('text_5'); ?>
+                <?php endif; ?>
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="layer"></div>
+            <div class="content">
+              <p>
+                <?php if (get_field('text_6')) : ?>
+                  <?php the_field('text_6'); ?>
+                <?php endif; ?>
+              </p>
+            </div>
+          </div>
+          <div class="card recital-title ravel">
+            <div class="layer"></div>
+            <div class="content">
+              <p>
+                <?php if (get_field('text_7')) : ?>
+                  <?php the_field('text_7'); ?>
+                <?php endif; ?>
+              </p>
+            </div>
+          </div>
+          <div class="spacer"></div>
         </div>
       </div>
     </div><!-- .entry-content -->
